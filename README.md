1196c74c3fc193c803279310a0806912e054b7d8
peppo.asap <ettorre19@gmail.com>
2020-06-14 19:43:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Echo Client
+
+Echo Client is a client-side JS library to test WebSocket with Echo Messages.
+
+## Installation
+
+### Manual Download
+Download the echo.js file and add it to your project folder.
+
+```html
+<script src="echo.js"></script>
+```
+
+### Using NPM
+Use the package manager [npm](https://www.npmjs.com/get-npm) to install Echo Client.
+
+```js
+npm install echo-client
+```
+
+You need to use Express Server to serve the file.
+Add this line to your Express Server.
+
+```
+app.use("/echo", express.static(__dirname + "/node_modules/echo-client"));
+```
+Next add it to your HTML.
+
+```html
+<script src="/echo/echo.js"></script>
+```
+
+## Usage
+
+```js
+//Create a Echo providing the WebSocket Echo Server URL
+//This one is good - "wss://echo.websocket.org"
+let echo = new Echo("wss://echo.websocket.org");
+
+//Set the Callback to execute when receiving a message from the server. 
+echo.whenReply((msg) => {
+    console.log(msg.data);
+});
+
+//generate(object, n)
+//This method send n messages with object as content to Echo Server
+//You will receive the same messages.
+echo.generate({hello: "Hello World!"}, 2);
+
+//generateEverySecond(object, second, n)
+//This method send n messages with object as content to Echo Server
+//and repeat this every setted second/s.
+echo.generateEverySecond({test: "I am looping every 1 second"}, 1, 5);
+
+//Stop the generateEverySecond method if loop is running.
+echo.stop();
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ echo.js
@@ -0,0 +1,65 @@
+
+class Echo {
+
+    constructor(url) {
+        this._url = url;
+        this._socket = new WebSocket(url);
+        console.log(`Socket pointing to: ${url}`);
+    }
+
+    waitForOpenConnection(socket) {
+        return new Promise((resolve, reject) => {
+            const maxNumberOfAttempt = 10;
+            const intervalTime = 200;
+
+            let currentAttempt = 0;
+            const interval = setInterval(() => {
+                if (currentAttempt > maxNumberOfAttempt - 1) {
+                    clearInterval(interval);
+                    reject(new Error('Maximum number of attempts.'));
+                } else if (socket.readyState === socket.OPEN) {
+                    clearInterval(interval);
+                    resolve();
+                }
+                currentAttempt++;
+            }, intervalTime);
+        });
+    }
+
+    async send(data) {
+        if (this._socket.readyState !== WebSocket.OPEN) {
+            try {
+                await this.waitForOpenConnection(this._socket);
+                this._socket.send(data);
+            } catch (err) {
+                console.error(err);
+            }
+        } else {
+            this._socket.send(data);
+        }
+    }
+
+    generate(object, reqNumber){
+        for (let i = 0; i < reqNumber; i++){
+          this.send(JSON.stringify(object));
+        }
+    }
+
+    generateEverySecond(object, second, numberOfMessage) {
+        this._loop = setInterval(() => {
+            this.generate(object, numberOfMessage);
+        }, second * 1000);
+    }
+
+    stop() {
+        if (this._loop !== null) {
+            clearInterval(this._loop);
+        }
+    }
+
+    
+    whenReply(callback){
+        this._socket.onmessage = callback;
+    }
+
+}
